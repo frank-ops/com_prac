398ca16d0a6e98a2c25b7c8234a92f866940044e
frank-ops <thejasfranklin@gmail.com>
2022-12-07 15:21:45 +0530

    cherry 1

--- 2nd_max.cpp
+++ 2nd_max.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
   int arr[100] , n , val1=INT_MIN , val2=INT_MIN ;
-  cout<<"enter the size : (size should be 0 < n <= 2000 )";
+  cout<<"enter the size : (size should be 0 < n <= 200 )";
   cin>> n;
   for(int  i=0 ; i<n ; i++)
     cin>> arr[i];
